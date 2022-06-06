19ba8683976127e8564ad0df82d2d4b4c110ac69
ss <setrotec@outlook.com>
2022-06-06 19:39:34 -0300

    iniciando treinamento em Golang, hello mundinho!

---
+++ hello.go
@@ -0,0 +1,10 @@
+//pacote principal
+package main
+
+//importando pacote de formataçao (impressão e formatação de saidas)
+import "fmt"
+
+//função principal
+func main() {
+	fmt.Println("Olá mundo go! Dando salve salve!")
+}
